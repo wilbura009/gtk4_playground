d41e26a22787100bb02eb08ab8033c472aa97cf6
wilbura009 <57925027+wilbura009@users.noreply.github.com>
2023-05-23 20:26:37 -0500

    Update update-rm.sh

--- sh/update-rm.sh
+++ sh/update-rm.sh
@@ -17,7 +17,7 @@ date:     $TIME_STAMP
 
 # gtk4 playground
 
-This is a playground for gtk4. It is a collection of projects, examples, and information about developing gtk4 applications.
+This is a collection of projects, examples, and information about developing gtk4 applications.
 
 ## Projects
 " > $ROOT_README
